a3b3550ef12993c0126e78df710edafa8e63837e
urraccon <133591634+urraccon@users.noreply.github.com>
2024-08-07 13:03:10 +0300

    updated the vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
 
-  base: "/TaskPro/",
+  base: "/taskpro-frontend",
 
   build: {
     outDir: "dist",
